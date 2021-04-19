76753670cb810af5ba559a4d36dd4ce0e97bea05
martapanda <marta.urbowska@gmail.com>
2021-04-19 20:41:02 +0200

    uploaded script

---
+++ script.js
@@ -0,0 +1,15 @@
+var dice = ["images/dice1.png";
+"images/dice2.png";
+"images/dice3.png";
+"images/dice4.png";
+"images/dice5.png";
+"images/dice6.png";]
+var randomDice = Math.random(dice);
+
+function run() {
+  document.querySelector("img1").setAttribute("src", randomDice);
+  document.querySelector("img2").setAttribute("src", randomDice);
+
+}
+
+function run();
